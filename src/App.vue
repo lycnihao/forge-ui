1f404ab2fd4d2e5580a6c6e80c45acdbb1466c64
liyc <38707145@qq.com>
2023-04-28 11:42:20 +0800

    国际化配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <a-style-provider hash-priority="high">
-    <RouterView />
-  </a-style-provider>
+  <a-config-provider :locale="locale">
+    <a-style-provider hash-priority="high">
+      <RouterView />
+    </a-style-provider>
+  </a-config-provider>
 </template>
+<script lang="ts">
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+export default {
+  data() {
+    return {
+      locale: zhCN,
+    };
+  },
+};
+</script>
